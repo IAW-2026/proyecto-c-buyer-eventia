1f2c421ea9b46d0beec3785488c9e046e39b59eb
Clara Ithurrart <claraithurrart3@gmail.com>
2026-05-09 13:21:06 -0300

    integracion de prisma y PostgreQSL

---
+++ lib/prisma.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL as string,
+});
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter }); // si ya existe prisma lo uso, sino lo creo
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;

f9e217cd58c4d8e012b341034ca08f93312abe69
Clara Ithurrart <claraithurrart3@gmail.com>
2026-06-23 16:16:12 -0300

    cambio api

--- app/api/buyer/datos/entradaDevuelta/route.ts
+++ app/api/buyer/datos/entradaDevuelta/route.ts
@@ -10,12 +10,17 @@ export async function POST(request: Request) {
             return NextResponse.json({ error: 'No autorizado' }, { status: 401 });
         }
     const body = await request.json();
-    const idPedido = body.idPedido;
+    
+    // conversión a número por si llega como string
+    const idPedido = body.idPedido ? Number(body.idPedido) : NaN;
 
-    if (!idPedido || typeof idPedido !== 'number') {
-      return NextResponse.json(null, { status: 400 });
+    // Validar que sea un número válido y no un NaN (Not a Number)
+    if (isNaN(idPedido)) {
+      return NextResponse.json(
+        { error: 'El campo idPedido es requerido y debe ser un número válido.' }, 
+        { status: 400 }
+      );
     }
-
     await cancelarPedido({ idPedido }); //uso server action
 
     return NextResponse.json(
